afbd256890477d35af1b36556f4c07fd60f050dc
Cody Mathis <cody@patchmypc.com>
2024-12-02 13:35:40 -0500

    day 2

---
+++ Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC2024
+{
+    public class Helper
+    {
+        public static string[] GetInput(int day)
+        {
+            var filePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), $@"Day{day}\input.txt");
+            return File.ReadAllLines(filePath);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿using System.Diagnostics;
 using System.Reflection;
+using AOC2024;
 
-var stopwatch = new Stopwatch();
-stopwatch.Start();
 
 var filePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Day1\input.txt");
-var lines = File.ReadAllLines(filePath);
+var lines = Helper.GetInput(1);
+var stopwatch = new Stopwatch();
+stopwatch.Start();
 
 List<int> left = new(lines.Length);
 List<int> right = new(lines.Length);
@@ -45,4 +46,99 @@ Console.WriteLine(totalSimilarity);
 stopwatch.Stop();
 
 Console.WriteLine("Total Runtime:");
-Console.WriteLine(stopwatch.ElapsedMilliseconds);
+Console.WriteLine(stopwatch.Elapsed);
+
+
+// Day 2
+filePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Day2\input.txt");
+lines = Helper.GetInput(2);
+stopwatch.Restart();
+var safeCount = 0;
+var unsafeLines = new List<string>();
+foreach(string line in lines)
+{
+    var report = line.Split([' ']).Select(int.Parse).ToList();
+    var orderedDictionary = new Dictionary<int, int>();
+    var step = 0;
+    foreach (var pair in report)
+    {
+        orderedDictionary.Add(step, pair);
+        step++;
+    }
+
+    var direction = orderedDictionary[0] < orderedDictionary[1] ? "Increasing" : "Decreasing";
+    if (orderedDictionary.All((Func<KeyValuePair<int, int>, bool>)Safe(report, direction)))
+    {
+        safeCount++;
+    }
+    else
+    {
+        unsafeLines.Add(line);
+    }
+}
+Console.WriteLine("Day2");
+Console.WriteLine("Part 1:");
+Console.WriteLine(safeCount);
+stopwatch.Stop();
+
+Console.WriteLine("Total Runtime:");
+Console.WriteLine(stopwatch.Elapsed);
+stopwatch.Restart();
+foreach (string line in unsafeLines)
+{
+    var report = line.Split([' ']).Select(int.Parse).ToList();
+    var orderedDictionary = new Dictionary<int, int>();
+    var step = 0;
+    foreach (var pair in report)
+    {
+        orderedDictionary.Add(step, pair);
+        step++;
+    }
+
+
+    for (int i = 0; i < orderedDictionary.Count; i++)
+    {
+        report = line.Split([' ']).Select(int.Parse).ToList();
+        report.RemoveAt(i);
+        var oneRemoved = orderedDictionary.Where(b => b.Key != i).ToDictionary();
+        var reindex = new Dictionary<int, int>();
+        var restep = 0;
+        foreach (var newpair in oneRemoved)
+        {
+            reindex.Add(restep, newpair.Value);
+            restep++;
+        }
+        var direction = reindex[0] < reindex[1] ? "Increasing" : "Decreasing";
+        if (reindex.All((Func<KeyValuePair<int, int>, bool>)Safe(report, direction)))
+        {
+            safeCount++;
+            break;
+        }
+
+    }
+}
+Console.WriteLine("Day2");
+Console.WriteLine("Part 2:");
+Console.WriteLine(safeCount);
+stopwatch.Stop();
+
+Console.WriteLine("Total Runtime:");
+Console.WriteLine(stopwatch.Elapsed);
+
+static Func<KeyValuePair<int, int>, bool> Safe(List<int> report, string direction)
+{
+    return e =>
+    {
+        var myIndex = e.Key;
+        if (myIndex == 0) { return true; }
+        var prevValue = report[myIndex - 1];
+        var diff = Math.Abs(e.Value - prevValue);
+        if (diff == 0)
+        {
+            return false;
+        }
+        var alwaysIncreasing = e.Value > prevValue && diff <= 3;
+        var alwaysDecreasing = e.Value < prevValue && diff <= 3;
+        return (alwaysDecreasing && direction == "Decreasing") || (alwaysIncreasing && direction == "Increasing");
+    };
+}
