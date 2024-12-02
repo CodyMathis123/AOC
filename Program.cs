78ceae8cf67edf38bcb150e8c42eb7142ccff8f2
Cody Mathis <cody@patchmypc.com>
2024-12-02 16:39:16 -0500

    basic day args

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
 ﻿using AOC2024;
+var daysToExecute = new List<int>();
+if (args.Length > 0)
+{
+    for (int i = 0; i < args.Length; i++)
+    {
+        daysToExecute.Add(int.Parse(args[i]));
+    }
+}
 
 var solutions = typeof(Program).Assembly.GetTypes().Where(t => !t.IsAbstract && typeof(AOCSolution).IsAssignableFrom(t)).ToArray();
 
@@ -10,5 +18,11 @@ var days = solutions
     .OrderBy(c => c?.Day);
 foreach (var day in days)
 {
-    day?.Solve();
+    if (daysToExecute.Count == 0 || daysToExecute.Contains(day.Day)) 
+    {
+        Console.WriteLine("--------------------------------------");
+        Console.WriteLine($"Day {day.Day}");
+        day?.Solve();
+        Console.WriteLine("--------------------------------------");
+    }
 }
